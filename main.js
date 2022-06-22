38a0427850512f5360fddb46d6da1fc3bc7fad3d
Brian Washington <bw49078@gmail.com>
2022-06-22 15:51:34 -0500

    added simple math functions

---
+++ main.js
@@ -0,0 +1,25 @@
+let x = 1;
+let y = 2;
+
+
+function add() {
+    sum = x + y;
+    console.log(sum);
+}
+
+function sub() {
+    sum = x - y;
+    console.log(sum);
+}
+
+function multiply() {
+    sum = x * y;
+    console.log(sum);
+}
+
+function divide() {
+    sum = x / y;
+    console.log(sum);
+}
+
+divide()
